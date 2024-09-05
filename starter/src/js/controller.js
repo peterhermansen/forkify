45809eb1787f8f552c99b85ca2b69b641a6b2ce2
peterhermansen <hermansenph@gmail.com>
2024-09-05 17:59:03 -0400

    Added new welcome feature

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -111,6 +111,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -119,5 +123,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
+  newFeature();
 };
 init();
