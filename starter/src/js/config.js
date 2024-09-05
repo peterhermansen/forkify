a33d32a7682d78ff3e7268b0b6cee2788bf7f234
peterhermansen <hermansenph@gmail.com>
2024-09-05 17:48:08 -0400

    Initial commit

---
+++ starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'dc510e8a-fdd3-442c-a823-c845b09d3759';
+export const MODAL_CLOSE_SEC = 1;
